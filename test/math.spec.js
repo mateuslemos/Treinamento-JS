85b879dd00902151850b5246c0fbb005188df892
Mateus Lemos <mts.lemos@gmail.com>
2020-07-30 21:44:26 -0300

    MOCHA
    - Uso de hooks (beforeEach) para procedimentos repetitivos;

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,6 +1,13 @@
 const assert = require('assert');
 const Math = require('../src/math.js');
 
+let value = 1;
+
+//Hooks do Mocha
+beforeEach(function () {
+    value = 1;
+});
+
 describe('Math class', function(){
   it('Soma dois números', function(){
       const math = new Math();
@@ -10,7 +17,7 @@ describe('Math class', function(){
 
     it('Soma três números', function (done) {
         const math = new Math();
-        let result = math.sumThreeNumbers(5, 3, 1, (value) => {
+        let result = math.sumThreeNumbers(5, 3, value, (value) => {
             assert.equal(value, 9);
             done();
         });
@@ -19,19 +26,18 @@ describe('Math class', function(){
     it('Multiplica dois números');
 
     it.only('Divide dois números', function () {
-        let NUMBER1 = 1;
+        value = 2;
         let NUMBER2 = 2;
 
-        let result = NUMBER2 / NUMBER1;
+        let result = NUMBER2 / value;
 
-        assert.equal(result, 2);
+        assert.equal(result, 1);
     });
 
     it.skip('Subtrai dois numeros', function () {
-        let NUMBER1 = 1;
         let NUMBER2 = 2;
 
-        let result = NUMBER2 - NUMBER1;
+        let result = NUMBER2 - value;
 
         assert.equal(result, 1);
     });
